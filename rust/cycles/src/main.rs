8a513a391bd174cfa23f9c4bf01b86aff01008ad
Graham Cummins <grahamiancummins@gmail.com>
2018-03-19 13:33:25 -0600

    rust sync

--- rust/cycles/src/main.rs
+++ rust/cycles/src/main.rs
@@ -8,19 +8,22 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 struct Graph {
-    data: HashMap<i32, Vec<i32>>
+    data: HashMap<i32, Vec<i32>>,
+    empty: Vec<i32>
 
 }
 
 impl Graph {
+
     fn read(file: String) -> Graph {
         env_logger::init();
+        let empty = vec![];
         let mut data = HashMap::new();
         let f = match File::open(&file) {
             Ok(v) => v,
             Err(_e) => {
                 error!("failed to load file {}", file);
-                return Graph { data };
+                return Graph { data, empty };
             }
         };
         let buffered = BufReader::new(f);
@@ -31,12 +34,18 @@ impl Graph {
                 _ => info!("no line"),
             }
         }
-
-        return Graph { data }
+        return Graph { data, empty }
     }
 
     fn size(&self) -> usize {
         self.data.len()
+    }
+
+    fn edges(&self, vertex: i32) -> &Vec<i32> {
+        match self.data.get(&vertex) {
+            Some(v) => &v,
+            None => &(self.empty)
+        }
     }
 }
 
@@ -71,7 +80,7 @@ fn main() {
     let ref mode = if args.len() > 2 {
         &args[2]
     } else {
-        "async"
+        "sync"
     };
     let path = format!("../../data/graph{}.adj", n);
     let graph = Graph::read(path);
@@ -82,14 +91,33 @@ fn main() {
 }
 
 fn find_cycles_sync(graph: Graph) {
-    println!("graph size {}", graph.size())
-
+    println!("graph size {}", graph.size());
+    let mut n = 0;
+    for k in graph.data.keys() {
+        n = n + count_cycles_from(vec![k], &graph);
+    };
+    println!("found {} cycles", n);
 }
 
 fn find_cycles(graph: Graph) {
-    println!("graph size {}", graph.size())
+    println!("graph size {}", graph.size());
 }
 
+fn count_cycles_from(path: Vec<&i32>, graph: &Graph) -> i32 {
+    let mut n = 0;
+    let end: i32 = *path[path.len() - 1];
+    let start: i32 = *path[0];
+    for tid in graph.edges(end) {
+        if tid == &start {
+            n = n + 1;
+        } else if tid > &start && !path.contains(&&tid) {
+            let mut np = path.clone();
+            np.push(&tid);
+            n = n + count_cycles_from(np, graph);
+        };
+    };
+    n
+}
 
 
 
