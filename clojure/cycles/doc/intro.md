7c2b53b2279477d135984152ab684aa3347670ab
Graham Cummins <grahamiancummins@gmail.com>
2018-03-23 08:59:16 -0600

    adding a clojure app

---
+++ clojure/cycles/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cycles
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/cycles/test/cycles/core_test.clj
@@ -0,0 +1,7 @@
+(ns cycles.core-test
+  (:require [clojure.test :refer :all]
+            [cycles.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
