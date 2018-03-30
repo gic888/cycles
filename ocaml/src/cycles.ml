0b8cb74f2eb07c9a2803932a7ecbcc286c94139d
Graham Cummins <grahamiancummins@gmail.com>
2018-03-30 15:45:33 -0600

    OMG do I ever hate the ocaml ecosystem

--- ocaml/src/cycles.ml
+++ ocaml/src/cycles.ml
@@ -1,2 +1,12 @@
-print_string "hi"
+open Core.Std;;
+
+let read_file (filename : string) : string list =
+  In_channel.read_lines filename 
+
+let () = 
+  let nargs = Array.length Sys.argv in 
+  let n = if nargs > 1 then Sys.argv.(1) else "20" in 
+  let fn = Printf.sprintf "../data/graph%s.adj" n in 
+  let lines = read_file fn in 
+  print_newline (print_int (List.length lines)); 
 
