14c552c5d64987272588f95f4833d8e94b31e542
Graham Cummins <grahamiancummins@gmail.com>
2018-03-30 13:24:36 -0600

    trying parallel

--- haskell/cycles/src/Lib.hs
+++ haskell/cycles/src/Lib.hs
@@ -3,39 +3,44 @@ module Lib
     ) where
 
 import qualified Data.Map as M
-import qualified Data.List.Split as S
+import qualified Data.List.Split as Spl
+import qualified Data.Set as S
+import Control.Parallel (par) 
 
 type Vid = String
 type Graph = M.Map Vid [Vid]
-type Path = (Vid, [Vid], Graph)
-
-endOf:: Path -> Vid
-endOf (s, [], _) = s
-endOf (_, h : t, _) = h
+type Path = (Vid, Vid, S.Set Vid, Graph)
 
 nextFrom :: Path -> [Vid]
-nextFrom p@ (_, _, g) = case M.lookup (endOf p) g of
+nextFrom p@ (_, e, _, g) = case M.lookup e g of
   Just l -> l
   Nothing -> []
 
+pCount :: (Vid -> Int) -> [Vid] -> Int
+pCount f [] = 0
+pCount f (x : xs) =  r `par` r + pCount f xs
+  where r = (f x) `using` rdeepseq
 
 readGraph :: String -> Graph
 readGraph content = M.fromList [(head lw, tail lw) |
   l <- lines content,
-  let lw =  S.splitOn " " l ,
+  let lw =  Spl.splitOn " " l ,
   length l > 0,
   head l /= '#']
 
+sCount :: Graph -> Int
+sCount graph = sum [countCyclesFrom (x, x, S.empty, graph) | x <- M.keys graph]
+
 countCycles :: Graph -> Int
-countCycles graph = sum [countCyclesFrom (x, [], graph) | x <- M.keys graph]
+countCycles graph = pCount (\x -> countCyclesFrom (x, x, S.empty, graph))  (M.keys graph)
 
 countCyclesFrom :: Path -> Int
 countCyclesFrom p = sum [countCyclesAt x p | x <- nextFrom p]
 
 countCyclesAt :: Vid -> Path -> Int
-countCyclesAt v p@(s, m, g)
+countCyclesAt v p@(s, e, m, g)
   | v == s = 1
-  | v > s && not (elem v m) = countCyclesFrom (s, v : m, g)
+  | v > s && not (S.member v m) = countCyclesFrom (s, v, S.insert v m, g)
   | otherwise = 0
 
 
