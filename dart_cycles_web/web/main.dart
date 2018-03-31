0cffdb8fc01b4d3c8f09fb77ca0e8adb6d5d9eed
Graham Cummins <grahamiancummins@gmail.com>
2018-03-31 10:34:35 -0600

    dart web

--- dart_cycles_web/web/main.dart
+++ dart_cycles_web/web/main.dart
@@ -1,50 +1,6 @@
 import 'dart:html';
 import 'dart:async';
 
-void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
-}
-
-Future<int> count_cycles(Map<String, List<String>> g) async {
-  return Future
-      .wait(g.keys.map( (v) => _cycles_starting_at(v, g)))
-      .then((List<int> counts) => _lsum(counts));
-}
-
-Future<int> _cycles_starting_at(String v, Map<String, List<String>> g) async {
-  return _lsum(g[v].map((t) => _cycles_at(t, [v], g)));
-}
-
-int _lsum(Iterable<int> l) {
-  if (l.length == 0) {
-    return 0;
-  } else {
-    return l.reduce((i,j) => i + j);
-  }
-}
-
-int _cycles_at(String v, List<String> path, Map<String, List<String>> g) {
-  if (v == path[0]) {
-    return 1;
-  } else if (v.compareTo(path[0]) > 0 && !path.contains(v) && g.containsKey(v) && !g[v].isEmpty) {
-    var np = [];
-    np.addAll(path);
-    np.add(v);
-    return _lsum(g[v].map((t) => _cycles_at(t, np, g)));
-  } else {
-    return 0;
-  }
-}
-
-Map<String, List<String>> read(String data) {
-  var raw_lines = data.split("\n");
-  var lines = raw_lines
-      .map((s) => s.trim())
-      .where((s) => s.length > 0 && !s.startsWith("#"))
-      .map((s) => s.split(" "));
-  return new Map.fromIterable(lines,
-      key: (l) => l[0], value: (List<String> l) => l.sublist(1, l.length));
-}
 
 
 const g20 = '''
@@ -107,3 +63,65 @@ const g35 = '''
 33 32
 34 13 17 22
 ''';
+
+typedef Report(String);
+
+void main() {
+  var o1 = querySelector('#output');
+  o1.text = 'Ready.';
+  var b20 = querySelector('#g20b') as ButtonElement;
+  var b35 = querySelector('#g35b') as ButtonElement;
+  var rep = (s) => o1.text = s;
+  b20.onClick.listen((evt) => report(g20, rep));
+  b35.onClick.listen((evt) => report(g35, rep));
+}
+
+Future report(String data, Report r) async {
+  var start = new DateTime.now();
+  var n = await count_cycles(read(data));
+  var stop = new DateTime.now();
+  var t = stop.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
+  var s = "Counted $n cycles in $t ms";
+  r(s);
+}
+
+Future<int> count_cycles(Map<String, List<String>> g) async {
+  return Future
+      .wait(g.keys.map( (v) => _cycles_starting_at(v, g)))
+      .then((List<int> counts) => _lsum(counts));
+}
+
+Future<int> _cycles_starting_at(String v, Map<String, List<String>> g) async {
+  return _lsum(g[v].map((t) => _cycles_at(t, [v], g)));
+}
+
+int _lsum(Iterable<int> l) {
+  if (l.length == 0) {
+    return 0;
+  } else {
+    return l.reduce((i,j) => i + j);
+  }
+}
+
+int _cycles_at(String v, List<String> path, Map<String, List<String>> g) {
+  if (v == path[0]) {
+    return 1;
+  } else if (v.compareTo(path[0]) > 0 && !path.contains(v) && g.containsKey(v) && !g[v].isEmpty) {
+    var np = [];
+    np.addAll(path);
+    np.add(v);
+    return _lsum(g[v].map((t) => _cycles_at(t, np, g)));
+  } else {
+    return 0;
+  }
+}
+
+Map<String, List<String>> read(String data) {
+  var raw_lines = data.split("\n");
+  var lines = raw_lines
+      .map((s) => s.trim())
+      .where((s) => s.length > 0 && !s.startsWith("#"))
+      .map((s) => s.split(" "));
+  return new Map.fromIterable(lines,
+      key: (l) => l[0], value: (List<String> l) => l.sublist(1, l.length));
+}
